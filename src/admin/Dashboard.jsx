4e83657fa9627cbfae73a5b3ffd653e6e7f147ab
Nazal_Malik <155678924+nazalmalik@users.noreply.github.com>
2025-07-11 10:28:49 +0500

    Update Dashboard.jsx

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import './dashboard.css';
+import './Dashboard.css';
 import {
   FaClipboardList,
   FaUsers,
