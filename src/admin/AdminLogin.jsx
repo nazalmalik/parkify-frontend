d3ce5772a705f2ca71319aa1dfc0c724f33f6707
Nazal_Malik <155678924+nazalmalik@users.noreply.github.com>
2025-07-11 12:57:26 +0500

    Update AdminLogin.jsx

--- src/admin/AdminLogin.jsx
+++ src/admin/AdminLogin.jsx
@@ -15,7 +15,7 @@ const AdminLogin = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:5000/api/admin/login', {
+      const response = await axios.post('https://parkify-backend-six.vercel.app/api/admin/login', {
         username: username.trim(),
         password: password.trim(),
       });
